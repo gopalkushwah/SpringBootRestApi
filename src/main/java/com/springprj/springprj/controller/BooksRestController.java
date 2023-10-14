cb7c85297006c1735ea358b50b1db432281c8839
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-10-14 18:40:04 +0530

    Sending Status code using ResponseEntity in SpringBoot RestApi and Testing it on Postman

--- src/main/java/com/springprj/springprj/controller/BooksRestController.java
+++ src/main/java/com/springprj/springprj/controller/BooksRestController.java
@@ -1,8 +1,11 @@
 package com.springprj.springprj.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,18 +24,32 @@ public class BooksRestController {
     private BookService bookService;
 
     @GetMapping("/books/{id}")
-    public Book getBooks(@PathVariable("id") int id) {
-        return this.bookService.getBookById(id);
+    public ResponseEntity<Book> getBooks(@PathVariable("id") int id) {
+        Book book = this.bookService.getBookById(id);
+        if (book == null) // if list empty then it will send status cod Not found 404
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.of(Optional.of(book)); // otherwise it will send 200 ok
     }
 
     @GetMapping("/books/all")
-    public List<Book> getAllBooks() {
-        return this.bookService.getAllBook();
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> list = this.bookService.getAllBook();
+        if (list.size() <= 0) // if list empty then it will send status cod Not found 404
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.of(Optional.of(list)); // otherwise it will send 200 ok
+
     }
 
     @PostMapping("/books")
-    public Book addBooks(@RequestBody Book book) {
-        return this.bookService.addBook(book);
+    public ResponseEntity<Book> addBooks(@RequestBody Book book) {
+        Book b = null;
+        try {
+            b = this.bookService.addBook(book);
+            return ResponseEntity.of(Optional.of(b));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
     @DeleteMapping("/books/{id}")
--- src/main/java/com/springprj/springprj/service/BookService.java
+++ src/main/java/com/springprj/springprj/service/BookService.java
@@ -25,7 +25,13 @@ public class BookService {
     }
 
     public Book getBookById(int id) {
-        return list.stream().filter(e -> e.getId() == id).findFirst().get();
+        Book book = null;
+        try {
+            book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return book;
     }
 
     public Book addBook(Book book) {
